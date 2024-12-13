639a6d4e0e096293c6bf755f198a81af0f047a40
Fazzioni <fazzioni@discente.ufg.br>
2024-12-12 22:24:49 -0300

    fix: update ball positioning logic in reset function for improved randomness

--- curriculum/reset.py
+++ curriculum/reset.py
@@ -9,14 +9,16 @@ def reset_random_one_ball(env,raio_buraco = 13):
     cfg.raio_buraco = raio_buraco
     
     epson = raio_buraco*2
-    cfg.bola_branca_posicao_inicial = torch.tensor([
-        torch.rand(1).item() * (env.table.largura - epson*2  ) + env.table.x_start + epson,
-        torch.rand(1).item() * (env.table.altura - epson*2  )  + env.table.y_start + epson
-    ])
 
     env.reset()
     env.table.bolas = env.table.bolas[1:2] + [env.table.bola_branca]
     numero_bola = env.table.bolas[0].numero
+    
+    
+    for _ ,  bola in enumerate(env.table.bolas):
+        bola.posicao[1] = torch.rand(1).item() * (env.table.altura - epson*2  )  + env.table.y_start + epson
+        bola.posicao[0] = torch.rand(1).item() * (env.table.largura - epson*2  ) + env.table.x_start + epson
+        
     env.numero_bolas = [[ numero_bola ], [ numero_bola ]]
     
     return env.get_observations()
