e9cfcc12cce717666b6690672b18a9099b91ad0b
Fazzioni <fazzioni@discente.ufg.br>
2024-12-16 09:41:32 -0300

    fix: update ball positioning logic in reset function

--- curriculum/reset.py
+++ curriculum/reset.py
@@ -11,14 +11,13 @@ def reset_random_one_ball(env,raio_buraco = 13):
     epson = raio_buraco*2
 
     env.reset()
-    env.table.bolas = env.table.bolas[1:2] + [env.table.bola_branca]
-    numero_bola = env.table.bolas[0].numero
-    
-    
-    for _ ,  bola in enumerate(env.table.bolas):
+    env.table.bolas = env.table.bolas[:2] + [env.table.bola_branca]
+    numero_bola = env.table.bolas[1].numero
+        
+    for _ ,  bola in enumerate(env.table.bolas[1:]):
         bola.posicao[1] = torch.rand(1).item() * (env.table.altura - epson*2  )  + env.table.y_start + epson
         bola.posicao[0] = torch.rand(1).item() * (env.table.largura - epson*2  ) + env.table.x_start + epson
         
-    env.numero_bolas = [[ numero_bola ], [ numero_bola ]]
-    
+    env.numero_bolas = [[ numero_bola ], [ 1 ]]
+        
     return env.get_observations()
